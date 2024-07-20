1846b8153b2e0e1757ced6d7fdd36997f7ebf17a
sh8ee <121178695+sh3ee@users.noreply.github.com>
2024-07-20 21:58:07 +0530

    removed: partials & intents:GuildMembers

--- structures/index.js
+++ structures/index.js
@@ -1,26 +1,14 @@
 const { CustomClient } = require('./classes/customClient.js');
 require('../lib/plugins/crashDefender.js').Shield();
 const appUtil = require('../lib/functions/application-ecs-loader.js');
-const { GatewayIntentBits, Partials } = require('discord.js');
+const { GatewayIntentBits } = require('discord.js');
 
-const client = new CustomClient(
-    {
-        intents: [
-            GatewayIntentBits.Guilds,
-            GatewayIntentBits.GuildMembers,
-            GatewayIntentBits.GuildMessages,
-            GatewayIntentBits.MessageContent
-        ],
-        partials: [
-            Partials.Channel,
-            Partials.Reaction,
-            Partials.User,
-            Partials.GuildMember,
-            Partials.Message,
-            Partials.ThreadMember
-        ],
-        failIfNotExists: false
-    }
-);
+const client = new CustomClient({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
 
 client.setMaxListeners(0); appUtil.loadEvents(client); client.start();
