24342e61cffd2de2d56a7e9bca7264d3bc4d0504
shishimaru <121178695+sh3ee@users.noreply.github.com>
2024-10-21 15:59:14 +0530

    remove file

--- src/lib/emojis/icons.js
+++
@@ -1,5 +0,0 @@
-const animated = { diamond: '<a:diamond:1112979860185432066>', gold: '<a:gold:1188754787404091432>' };
-const static = { right: '<:right:1118158799178567800>', wrong: '<:wrong:1118158803595182090>' };
-
-exports.static = static;
-exports.animated = animated;
